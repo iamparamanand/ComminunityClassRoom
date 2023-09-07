2d05a03dce8ef5c305c55f273fbdc988f3f5b3e2
Paramanand Kumar <paramanandkr@gmail.com>
2023-09-07 10:35:21 +0530

    First Java program

---
+++ Java/Basic/HelloWorld.java
@@ -0,0 +1,12 @@
+package Java.Basic;
+import java.util.*;
+// Java Hello World Program
+public class HelloWorld  {
+    /* This is the main function
+     * This is where the execution of 
+     * a Java Program begins
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World !!! Welcome to learn JAVA");
+    } 
+}
